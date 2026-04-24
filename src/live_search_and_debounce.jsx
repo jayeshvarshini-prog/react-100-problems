fafafb9ba70f280d2e8652b41d4f8ccf529ee9c3
jayeshvarshini-prog <jayeshvarshini@gmail.com>
2026-04-24 16:22:27 +0200

    loading text condition

--- src/live_search_and_debounce.jsx
+++ src/live_search_and_debounce.jsx
@@ -1,38 +1,39 @@
 import React, {useState, useEffect, useMemo} from 'react';
 import { WEATHER_API } from './utils/api';
 
-export default function App (){
-  
-  const [query, setQuery] = useState("");
-  const [loading, setLoading] = useState(false)
-  const [debounce, setDebounce] = useState('');
-  
-  useEffect(()=>{
-    setLoading(true)
-    const timer=setTimeout(()=>{
-        setDebounce(query);
-        setLoading(false);
-    },300)
-     return ()=> clearTimeout(timer) //arrow function
-  },[query]);
+export default function App(){
+    const [query, setQuery] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [debounce, setDebounce] = useState("");
 
-  const filterTemp = useMemo(()=>{
-    if(!debounce) return []; //question?
-    return WEATHER_API.filter((country)=>country.city.toLowerCase().includes(debounce.toLowerCase())); //carefull with the filter   
-  },[debounce]); //mindfull of the dependency
-   
-  return(
-    <>
-      <input type="text" value={query} onChange= {(e)=>setQuery(e.target.value)} placeholder="enter the text..." /> 
-      <p>{loading ? "loading.... " : ""}</p>
-      <ul>
-        {filterTemp.map((item)=>{ //don't  use debounce use filter
-          return <li key={item.id}> {item.city} - {item.temp}</li>
-        })}
-      </ul>
-      
-    </>)
-  
-} 
+    useEffect(()=>{
+        setLoading(true);
+        const timer= setTimeout(()=>{
+            setDebounce(query)
+            setLoading(false);
+        })
+        return ()=> clearTimeout(timer)
+    },[query])
+    
+    const filterClimate = useEffect(()=>{
+        return WEATHER_API.filter((country)=>{
+            const search = debounce.toLowerCase()
+            return country.city.toLowerCase().includes(search);       
+        })
 
+    },[debounce])
 
+    return(
+
+        <>
+        <input value={query} onChange={(e)=>setQuery(e.target.value)} placeholder="type the city.."/>
+        <p>{loading ? "loading" : ""}  </p>
+        <ul>
+            {filterClimate.map((items)=>{
+                return <li key={items.id}>{items.city} - {items.temp}</li>
+            }
+            )}
+        </ul>
+        </>
+    )
+}
