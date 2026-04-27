e062fdd68ab1ce0088b5ae44a4121d7d16513201
jayeshvarshini-prog <jayeshvarshini@gmail.com>
2026-04-27 16:46:29 +0200

    updated live search and debounce with useRef logic

--- src/live_search_and_debounce.jsx
+++ src/live_search_and_debounce.jsx
@@ -1,28 +1,42 @@
-import {useState, useEffect, useMemo} from 'react';
+import {useState, useEffect, useMemo, useRef} from 'react';
 import { WEATHER_API } from './utils/api';
 
-export default function App(){
+const LiveSearchAndDebounce = ()=>{
   const [query, setQuery] = useState("");
   const [debounce, setDebounce] = useState("");
   const [loading, setLoading] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+   const wrapperRef = useRef(null);
 
  useEffect(()=>{
   setLoading(true);
   const timer = setTimeout(()=>{
       setDebounce(query);
       setLoading(false);
-  },300)
-
+  },400)
   return ()=> clearTimeout (timer);
  },[query])  
+
+  useEffect(()=>{
+    const handleClickOutside = (event)=>{
+    if  (wrapperRef.current && !wrapperRef.current.contains(event.target)){
+      setIsOpen(false);
+    }
+    }
+    document.addEventListener("mousedown",handleClickOutside);
+     return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  },[])
 
   const filterData = useMemo(()=>{
     if(!debounce) return [];
     const search = debounce.toLowerCase();
     return WEATHER_API.filter((item)=>{
-      return item.city.toLowerCase().includes(debounce);
+      return item.city.toLowerCase().includes(search);
     })
   },[debounce])
+
 
   return(
     <>
@@ -30,14 +44,24 @@ export default function App(){
         type="text"
         value={query}
         placeholder="enter the text... "
-        onChange={(e)=>setQuery(e.target.value)}
+        onChange={(e)=>{setQuery(e.target.value); setIsOpen(!!e.target.value);} }
+        
       />
       <p>{loading ? "loading.. ": ""}</p>
-      <ul>
-        {filterData.map((item)=>(
-          <li key={item.id}>{item.city} - {item.temp}</li>
-        ))}
-      </ul>
+      <div ref={wrapperRef}>
+        {isOpen && (
+  <ul>
+    {filterData.map((item) => (
+      <li key={item.id}>
+        {item.city} - {item.temp}
+      </li>
+    ))}
+  </ul>
+)}
+      </div>
+      
     </>
   )
 }
+
+export default LiveSearchAndDebounce;
